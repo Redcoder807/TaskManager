c9cbd3735e2ba6829542e6c36e7ac292d20a103f
Redcoder807 <gearonhaynes@gmail.com>
2022-03-07 13:45:22 -0600

    TASKS Doesn't work

--- task.js
+++ task.js
@@ -3,12 +3,23 @@ let choice1;
 let taskon = true;
  const menu =`TASK MANAGER\n\nWhat would you like to do (Please enter one of the options below):\n"TASKS" - Displays All Tasks\n"NEW" - Add A New Task\n"REMOVE" - Remove A Task\n"CLOSE" - Close The Task Manager`
 
+const tasks = [
+    `Charge MacBook`,
+    `Master JavaScript`
+];
+
+let showTasks = ``;
 let userInput = prompt(menu);
 
 while (userInput !== `CLOSE`){
 
     if (userInput === `TASKS`){
-        alert(`You selected TASKS`)
+        for (task of tasks){
+            showTasks += `${task}\n`;
+        }
+        alert(showTasks);
+
+        showTasks = ``;
     }
 
     if (userInput === `NEW`){
